ba90ab99da1156a35da505f924427d0375a3fe75
Axel <git@simplyroba.de>
2023-06-11 19:29:58 +0200

    init from spring initializr

---
+++ src/main/kotlin/de/simplyroba/pixoobridge/PixooBridgeApplication.kt
@@ -0,0 +1,11 @@
+package de.simplyroba.pixoobridge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PixooBridgeApplication
+
+fun main(args: Array<String>) {
+	runApplication<PixooBridgeApplication>(*args)
+}
---
+++ src/test/kotlin/de/simplyroba/pixoobridge/PixooBridgeApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.simplyroba.pixoobridge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PixooBridgeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
