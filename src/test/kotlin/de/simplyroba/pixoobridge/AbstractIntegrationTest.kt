29320205bb5ee13bab9970a10295c0163c4fedf5
Axel Rojas Bachem <axel.bachem@rewe-digital.com>
2023-06-14 18:00:01 +0200

    wiremock

--- src/test/kotlin/de/simplyroba/pixoobridge/AbstractIntegrationTest.kt
+++ src/test/kotlin/de/simplyroba/pixoobridge/AbstractIntegrationTest.kt
@@ -3,9 +3,8 @@ package de.simplyroba.pixoobridge
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 @ActiveProfiles("test")
 abstract class AbstractIntegrationTest {
-
-
 }
