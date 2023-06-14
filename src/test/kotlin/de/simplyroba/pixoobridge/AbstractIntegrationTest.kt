925fa5bc97f2d5ed28c927769d1ee147a547668f
Axel Rojas Bachem <axel.bachem@rewe-digital.com>
2023-06-14 17:39:47 +0200

    webflux

--- src/test/kotlin/de/simplyroba/pixoobridge/AbstractIntegrationTest.kt
+++ src/test/kotlin/de/simplyroba/pixoobridge/AbstractIntegrationTest.kt
@@ -1,7 +1,11 @@
 package de.simplyroba.pixoobridge
 
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
+@SpringBootTest
 @ActiveProfiles("test")
 abstract class AbstractIntegrationTest {
+
+
 }
