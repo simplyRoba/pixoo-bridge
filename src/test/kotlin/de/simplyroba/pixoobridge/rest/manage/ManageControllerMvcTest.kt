1437ff307a88f5e6089a2eaa53be3d53ce397f71
Axel <git@simplyroba.de>
2023-06-18 11:49:20 +0200

    add white balance mock mvc test

--- src/test/kotlin/de/simplyroba/pixoobridge/rest/manage/ManageControllerMvcTest.kt
+++ src/test/kotlin/de/simplyroba/pixoobridge/rest/manage/ManageControllerMvcTest.kt
@@ -3,6 +3,7 @@ package de.simplyroba.pixoobridge.rest.manage
 import de.simplyroba.pixoobridge.rest.AbstractMvcTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
@@ -11,7 +12,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 class ManageControllerMvcTest: AbstractMvcTest() {
 
     @ParameterizedTest
-    @ValueSource(ints = [101, 200, -1, -50])
+    @ValueSource(ints = [101, -1])
     fun `should return bad request on brightness out of bound`(input: Int) {
         mockMvc.perform(post("/manage/display/brightness/$input"))
             .andExpect(status().isBadRequest)
@@ -24,15 +25,15 @@ class ManageControllerMvcTest: AbstractMvcTest() {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = [101, 123, -1, -24])
-    fun `should return bad request on red in white balance out of bound`(input: Int) {
+    @CsvSource(value = ["101:50:50", "-1:50:50", "50:101:50", "50:-1:50", "50:50:101", "50:50:-1"], delimiter = ':')
+    fun `should return bad request on white balance out of bound`(red: Int, green: Int, blue: Int) {
         mockMvc.perform(post("/manage/display/white-balance")
             .contentType(APPLICATION_JSON)
             .content("""
                 {
-                    "red": $input,
-                    "green": 50,
-                    "blue": 50
+                    "red": $red,
+                    "green": $green,
+                    "blue": $blue
                 }
             """.trimIndent()))
             .andExpect(status().isBadRequest)
