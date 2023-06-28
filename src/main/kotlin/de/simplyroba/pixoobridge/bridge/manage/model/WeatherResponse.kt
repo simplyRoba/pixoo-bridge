a9b0162979a7fbe95f907f52cd87ff366f79e9ac
Axel <git@simplyroba.de>
2023-06-28 22:16:55 +0200

    add more documentation to manage controller

--- src/main/kotlin/de/simplyroba/pixoobridge/bridge/manage/model/WeatherResponse.kt
+++ src/main/kotlin/de/simplyroba/pixoobridge/bridge/manage/model/WeatherResponse.kt
@@ -4,12 +4,12 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(name = "Weather")
 data class WeatherResponse(
-  val currentTemperature: Float,
-  val minimalTemperature: Float,
-  val maximalTemperature: Float,
+  @Schema(defaultValue = "33.68") val currentTemperature: Float,
+  @Schema(defaultValue = "31.68")val minimalTemperature: Float,
+  @Schema(defaultValue = "34.68")val maximalTemperature: Float,
   @Schema(allowableValues = ["Sunny", "Cloudy", "Rainy", "Rainy", "Frog"])
   val weatherString: String,
-  @Schema(description = "in hPa") val pressure: Int,
-  @Schema(description = "in %") val humidity: Int,
-  @Schema(description = "in m/s") val windSpeed: Float
+  @Schema(description = "in hPa", defaultValue = "1006") val pressure: Int,
+  @Schema(description = "in %", defaultValue = "47") val humidity: Int,
+  @Schema(description = "in m/s", defaultValue = "2.54") val windSpeed: Float
 )
