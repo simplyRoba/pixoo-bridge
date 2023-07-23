ec92ba748f337b8bb46a50f6ec9d2a088c4d49e4
Axel <git@simplyroba.de>
2023-07-23 14:13:35 +0200

    format

--- src/test/kotlin/de/simplyroba/pixoobridge/bridge/draw/DrawControllerMvcTest.kt
+++ src/test/kotlin/de/simplyroba/pixoobridge/bridge/draw/DrawControllerMvcTest.kt
@@ -8,22 +8,30 @@ import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
-class DrawControllerMvcTest: AbstractMvcTest() {
+class DrawControllerMvcTest : AbstractMvcTest() {
 
   @ParameterizedTest
-  @CsvSource(value = ["-1:0:0", "0:-1:0", "0:0:-1", "256:0:0", "0:256:0", "0:0:256"], delimiter = ':')
+  @CsvSource(
+    value = ["-1:0:0", "0:-1:0", "0:0:-1", "256:0:0", "0:256:0", "0:0:256"],
+    delimiter = ':'
+  )
   fun `should return bad request on rgb value out of bound`(red: Int, green: Int, blue: Int) {
-    mockMvc.perform(MockMvcRequestBuilders.post("/draw/fill")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(
-                    """
+    mockMvc
+      .perform(
+        MockMvcRequestBuilders.post("/draw/fill")
+          .contentType(MediaType.APPLICATION_JSON)
+          .content(
+            """
                       {
                         "red": $red,
                         "green": $green,
                         "blue": $blue
                       }
-                    """.trimIndent()))
-            .andExpect(MockMvcResultMatchers.status().isBadRequest)
+                    """
+              .trimIndent()
+          )
+      )
+      .andExpect(MockMvcResultMatchers.status().isBadRequest)
     Mockito.verifyNoInteractions(pixooClient)
   }
 }
--- src/test/kotlin/de/simplyroba/test/JsonUnitRegexBuilder.kt
+++ src/test/kotlin/de/simplyroba/test/JsonUnitRegexBuilder.kt
@@ -1,7 +1,8 @@
 package de.simplyroba.test
 
-//Inspired by https://github.com/lukas-krecan/JsonUnit/commit/75d68ef1852ade004e93ca42d676f4b996631974#diff-642a52fede8473f98c5a7b25f34c6bd68f33160cf22733127f7f7f13f3cb2fc6R713
-class JsonUnitRegexBuilder private constructor(private val expression: String)  {
+// Inspired by
+// https://github.com/lukas-krecan/JsonUnit/commit/75d68ef1852ade004e93ca42d676f4b996631974#diff-642a52fede8473f98c5a7b25f34c6bd68f33160cf22733127f7f7f13f3cb2fc6R713
+class JsonUnitRegexBuilder private constructor(private val expression: String) {
 
   companion object {
     @JvmStatic
