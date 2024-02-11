64c86ada12dcf7ee0894094e5c311521c9acbdc7
Axel Rojas Bachem <axel.bachem@rewe-digital.com>
2024-02-11 20:38:01 +0100

    implement reboot command

--- src/test/kotlin/de/simplyroba/pixoobridge/bridge/system/SystemControllerMvcTest.kt
+++ src/test/kotlin/de/simplyroba/pixoobridge/bridge/system/SystemControllerMvcTest.kt
@@ -1,4 +1,4 @@
-package de.simplyroba.pixoobridge.bridge.health
+package de.simplyroba.pixoobridge.bridge.system
 
 import de.simplyroba.pixoobridge.AbstractMvcTest
 import org.junit.jupiter.api.Test
@@ -7,7 +7,7 @@ import org.springframework.test.context.TestPropertySource
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 
 @TestPropertySource(properties = ["pixoo.health.forward=false"])
-class HealthControllerMvcTest : AbstractMvcTest() {
+class SystemControllerMvcTest : AbstractMvcTest() {
 
   @Test
   fun `should skip call to device when property set`() {
