64b4bc87a33572bd91223249f6e07b98e72e93c3
Axel Rojas Bachem <axel.bachem@rewe-digital.com>
2024-02-12 15:52:51 +0100

    take max file siye into account

--- src/main/kotlin/de/simplyroba/pixoobridge/util/FileDownloader.kt
+++ src/main/kotlin/de/simplyroba/pixoobridge/util/FileDownloader.kt
@@ -1,16 +1,23 @@
 package de.simplyroba.pixoobridge.util
 
+import de.simplyroba.pixoobridge.config.PixooConfig
 import org.springframework.core.io.ByteArrayResource
 import org.springframework.core.io.Resource
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 
 @Component
-class FileDownloader {
+class FileDownloader(private val pixooConfig: PixooConfig) {
   fun download(link: String): Resource {
 
+    val webClient = WebClient.builder()
+      .codecs { configurer ->
+        configurer.defaultCodecs().maxInMemorySize(pixooConfig.maxImageSize.toBytes().toInt())
+      }
+      .build()
+
     val bytes =
-      WebClient.create()
+      webClient
         .get()
         .uri(link)
         .retrieve()
