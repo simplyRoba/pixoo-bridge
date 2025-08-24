f6a5740912c9cfb08efa40af18138fb5320e017d
Axel Rojas Bachem <axel.rojasbachem@rewe-group.com>
2025-08-24 19:46:00 +0200

    fix: revert migration to rest client as it seems not working correctly

--- src/main/kotlin/de/simplyroba/pixoobridge/util/FileDownloader.kt
+++ src/main/kotlin/de/simplyroba/pixoobridge/util/FileDownloader.kt
@@ -1,50 +1,39 @@
 package de.simplyroba.pixoobridge.util
 
-import de.simplyroba.pixoobridge.client.PixooClient.Companion.DEFAULT_TIMEOUT
+import de.simplyroba.pixoobridge.config.PixooConfig
 import org.slf4j.LoggerFactory
 import org.springframework.core.io.ByteArrayResource
 import org.springframework.core.io.Resource
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.stereotype.Component
-import org.springframework.web.client.RestClient
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
 
 @Component
-class FileDownloader() {
+class FileDownloader(private val pixooConfig: PixooConfig) {
 
   private val logger = LoggerFactory.getLogger(javaClass)
 
   fun download(link: String): Resource {
 
-    val restClient =
-      RestClient.builder()
-        .requestFactory(
-          HttpComponentsClientHttpRequestFactory().apply {
-            setConnectTimeout(DEFAULT_TIMEOUT.inWholeMilliseconds.toInt())
-            setReadTimeout(DEFAULT_TIMEOUT.inWholeMilliseconds.toInt())
-          }
-        )
+    val webClient =
+      WebClient.builder()
+        .codecs { configurer ->
+          configurer.defaultCodecs().maxInMemorySize(pixooConfig.maxImageSize.toBytes().toInt())
+        }
         .build()
 
     val bytes =
-      restClient
+      webClient
         .get()
         .uri(link)
         .retrieve()
-        .onStatus(
-          { it.is4xxClientError || it.is5xxServerError },
-          { _, response ->
-            logger.error(
-              "Error while retrieving file: ${response.statusCode} - ${response.statusText}"
-            )
-            throw RemoteFileNotFoundException()
-          },
-        )
-        .body(ByteArray::class.java)
+        .bodyToMono(ByteArray::class.java)
+        .doOnError { e -> logger.warn("Error while retrieving file", e) } // log the error
+        // will return an empty byte array on http error like 404
+        .onErrorResume { _ -> Mono.empty() }
+        .block()
 
     if (bytes != null && bytes.isNotEmpty()) return ByteArrayResource(bytes)
-    else {
-      logger.error("Downloaded file was empty")
-      throw RemoteFileNotFoundException()
-    }
+    else throw RemoteFileNotFoundException()
   }
 }
