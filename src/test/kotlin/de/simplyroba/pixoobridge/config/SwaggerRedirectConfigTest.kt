0c59a426e73396c51fcf700db58ef86b32b2b26c
Axel Rojas Bachem <git@simplyroba.de>
2025-11-21 15:24:11 +0100

    feat: add root redirect to swagger ui (#300)

---
+++ src/test/kotlin/de/simplyroba/pixoobridge/config/SwaggerRedirectConfigTest.kt
@@ -0,0 +1,16 @@
+package de.simplyroba.pixoobridge.config
+
+import de.simplyroba.pixoobridge.AbstractMvcTest
+import org.junit.jupiter.api.Test
+import org.springframework.test.web.servlet.get
+
+class SwaggerRedirectConfigTest : AbstractMvcTest() {
+
+  @Test
+  fun `root path should permanently redirect to swagger-ui`() {
+    mockMvc.get("/").andExpect {
+      status { isPermanentRedirect() }
+      redirectedUrl("/swagger-ui/index.html")
+    }
+  }
+}
