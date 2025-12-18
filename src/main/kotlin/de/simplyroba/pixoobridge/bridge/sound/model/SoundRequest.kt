e03c607c4020f2837b45e24b1bc16a81466e334b
Axel <git@simplyroba.de>
2025-12-18 17:50:44 +0100

    docs: add comments to clarify sound timing parameters in SoundRequest

--- src/main/kotlin/de/simplyroba/pixoobridge/bridge/sound/model/SoundRequest.kt
+++ src/main/kotlin/de/simplyroba/pixoobridge/bridge/sound/model/SoundRequest.kt
@@ -4,6 +4,9 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(name = "Sound")
 data class SoundRequest(
+  // Times are in ms. There are no official limits documented, these are just reasonable
+  // assumptions. e.g. the active cycle should be at least 100ms to be audible. Sound playing longer
+  // than 100s seems unreasonable.
   @param:Schema(minimum = "100", maximum = "10000") val activeTimeInCycle: Int,
   @param:Schema(minimum = "0", maximum = "10000") val offTimeInCycle: Int,
   @param:Schema(minimum = "100", maximum = "100000") val totalPlayTime: Int,
