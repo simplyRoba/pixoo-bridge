b4962090b2662069595cb58a43f12a9e847ae229
Axel <git@simplyroba.de>
2025-12-19 10:18:30 +0100

    refactor: replace WebClient with RestClient for file downloads and enhance error handling

--- src/main/kotlin/de/simplyroba/pixoobridge/util/RemoteFileNotFoundException.kt
+++ src/main/kotlin/de/simplyroba/pixoobridge/util/RemoteFileNotFoundException.kt
@@ -4,4 +4,5 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Could not download remote file")
-class RemoteFileNotFoundException : RuntimeException() {}
+class RemoteFileNotFoundException(message: String? = null, cause: Throwable? = null) :
+  RuntimeException(message, cause)
